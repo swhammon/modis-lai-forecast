69a356438ab0ac2388d29a57cd68c73423491dc0
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2023-11-14 12:33:12 -0500

    somewhat roundabout way to prevent copying directory name into hive structure

--- R/spat4cast_submit.R
+++ R/spat4cast_submit.R
@@ -5,13 +5,11 @@
 #' @param variable The data source being forecasted. Currently "lai_recovery" is the only option.
 #' @param site_id The side ID of the area being forecasted. Currently "august_complex" is the only option.
 #'
-#' @return
+#' @return message from minio submission
 #' @export
 #'
-#' @examples
-exported <- function(x) {
-  x + 1
-}
+#' @examples spat4cast_submit(dir = "climatology", model_id = "team1-climatology", variable = "lai_recovery", site_id = "august_complex")
+
 spat4cast_submit <- function(dir, 
                              model_id,
                              variable = c("lai_recovery"), 
@@ -29,17 +27,19 @@ spat4cast_submit <- function(dir,
   # Check metadata for files
   ## can we use the `variable` and `site_id` arguments to pull a template target file for comparison?
   ## if we are to enforce these attributes, then we should modify `ingest_planetary_data` to remove dy, dx, dt, srs arguments (and have them enforced according to site_id)
-  submitted_files_rast <- rast(submitted_files)
-  res(submitted_files_rast)
-  origin(submitted_files_rast)
-  ext(submitted_files_rast)
-  crs(submitted_files_rast)
+  submitted_files_rast <- terra::rast(submitted_files)
+  terra::res(submitted_files_rast)
+  terra::origin(submitted_files_rast)
+  terra::ext(submitted_files_rast)
+  terra::crs(submitted_files_rast)
   
   # Upload anonymously to submissions bucket, using hive partitioning for file structure
-  out <- glue::glue("efi/spat4cast-submissions/variable={variable}/site_id={site_id}/model_id={model_id}") 
-  
   minioclient::mc_alias_set("efi", "data.ecoforecast.org", access_key = "", secret_key = "") # keys not needed for public bucket submission
-  submit <- minioclient::mc_cp(dir, out, recursive = TRUE)
+  
+  file_names <- basename(submitted_files)
+  out <- glue::glue("efi/spat4cast-submissions/variable={variable}/site_id={site_id}/model_id={model_id}/") 
+  
+  submit <- purrr::walk(file_names, ~withr::with_dir(dir, minioclient::mc_cp(from = ., out, recursive = TRUE)))
   
   # Return submit message (or maybe something more succinct?)
   return(submit)
