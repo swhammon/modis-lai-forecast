2b3f09e1eb2518742a7a156f9448767f607fddfc
Carl Boettiger <cboettig@gmail.com>
2023-11-26 09:53:53 -0800

    Update spat4cast_submit.R
    add reference date as @rqthomas suggests

--- R/spat4cast_submit.R
+++ R/spat4cast_submit.R
@@ -37,7 +37,7 @@ spat4cast_submit <- function(dir,
   minioclient::mc_alias_set("efi", "data.ecoforecast.org", access_key = "", secret_key = "") # keys not needed for public bucket submission
   
   file_names <- basename(submitted_files)
-  out <- glue::glue("efi/spat4cast-submissions/variable={variable}/site_id={site_id}/model_id={model_id}/") 
+  out <- glue::glue("efi/spat4cast-submissions/variable={variable}/site_id={site_id}/model_id={model_id}/reference_date={reference_date}") 
   
   submit <- purrr::walk(file_names, ~withr::with_dir(dir, minioclient::mc_cp(from = ., out, recursive = TRUE)))
   
