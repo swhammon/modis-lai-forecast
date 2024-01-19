3f3264def4784b6492c1321f10668b99e3671f27
ddurden <daviddurden27@gmail.com>
2024-01-19 20:06:19 +0000

    - updating target generation

--- targets/spatial_targets.R
+++ targets/spatial_targets.R
@@ -25,23 +25,24 @@ library(sf)
 library(lubridate)
 library(gdalcubes)
 library(rstac)
+library(stars)
 
 
 #Source functions
 for (f in list.files(here::here("R"), full.names = TRUE)) source (f)
 
 #Create fire bounding box
-fire_box <- fire_bbox(fire = "august_complex", pad_box = TRUE)
+fire_box <- fire_bbox(fire = site_id, pad_box = TRUE)
 
 #Target date
-date <- as.Date(Sys.time())
+date <- lubridate::floor_date(as.Date(Sys.time()), "month") #first day of the month
 
 # Ingest data ------------------------------------------------------------
 gdalcubes::gdalcubes_options(parallel=TRUE)
 
 # use ingest_planetary_data function to extract raster cube for fire bounding box between Jan 1 2002 and July 1 2023.
-raster_cube <- ingest_planetary_data(start_date = date - months(2), 
-                                     end_date = date + months(1), 
+raster_cube <- ingest_planetary_data(start_date = date - months(1), 
+                                     end_date = date, 
                                      box = fire_box$bbox,
                                      srs = "EPSG:4326",
                                      dx = dx, 
@@ -53,9 +54,11 @@ raster_cube <- ingest_planetary_data(start_date = date - months(2),
 
 
 # create target file
-target <- create_target_file(cuberast = raster_cube,
-                             date = date,
-                             dir = "/vsis3/spat4cast-targets",
+  target <- create_target_file(cuberast = raster_cube,
+                               site_id = site_id,
+                               date = as.character(date),
+                             dir = tempdir(),
+                             bucket = "efi/spat4cast-targets",
                              mask = fire_box$maskLayer)
                              
                              
